0c72e9de85afc6d069bdee0d52f97f2425146bfc
Ana Paula Barros <barros.anapaulas@gmail.com>
2015-08-31 10:14:07 -0300

    retirando import nao usados: Arrays na classe System

--- Rede_Plus_Pop/src/core/SystemPop.java
+++ Rede_Plus_Pop/src/core/SystemPop.java
@@ -2,7 +2,6 @@ package core;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
