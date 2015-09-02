044232db85dfb5bce693dfcbde6cf726d88a4378
Ana Paula Barros <barros.anapaulas@gmail.com>
2015-09-02 17:20:18 -0300

    contador de hasTags adicionado

--- Rede_Plus_Pop/src/core/SystemPop.java
+++ Rede_Plus_Pop/src/core/SystemPop.java
@@ -1,12 +1,17 @@
 package core;
 
+import java.security.KeyStore.Entry;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
@@ -442,7 +447,8 @@ public class SystemPop {
 	}
 	
 	public Map<String, Integer> getRankigHastagsMais() {
-		
 		return contadorDeHastags;
 	}
+	
+
 }
