0c4f3a8149fcf9b599f867d01f26537ccb348d4e
Ana Paula Barros <barros.anapaulas@gmail.com>
2015-09-16 18:59:54 -0300

    documentacao das classses: parte 2

--- Rede_Plus_Pop/src/core/SystemPop.java
+++ Rede_Plus_Pop/src/core/SystemPop.java
@@ -38,6 +38,13 @@ public class SystemPop {
 	private Map<String, ArrayList<String>> dicionarioHashtags;
 	private Map<String, Integer> contadorDeHastags;
 	
+	/**
+	 * construtor da classe SystemPop, ela é o controller do projeto
+	 * entao cabe a ela toda gerencia e administracao de dados em que
+	 * o sistema manipula ao longo do seu funcionamento
+	 * 
+	 * @author Ana Paula Barros e Gabriela Mota - UFCG : Computacao
+	 */
 	public SystemPop() {
 		usuarios = new ArrayList<Usuario>();
 		usuarioLogado = null;
@@ -45,6 +52,17 @@ public class SystemPop {
 		contadorDeHastags = new HashMap<String, Integer>();
 	}
 	
+	/**
+	 * Metodo para cadastrar um novo usuario no +Pop
+	 * 
+	 * @param String - nome
+	 * @param String - email
+	 * @param String - senha
+	 * @param String - dataNasc
+	 * @param String - imagem
+	 * @return String - email
+	 * @throws Exception
+	 */
 	public String cadastraUsuario(String nome, String email, String senha, String dataNasc, String imagem) throws Exception {
 		Usuario novoUsuario = new Usuario(nome, email, senha, dataNasc, imagem);
 		if (getUsuarios().contains(novoUsuario))
@@ -53,6 +71,13 @@ public class SystemPop {
 		return email;
 	}
 
+	/**
+	 * Metodo para um usuario ja cadastrado, realizar login na rede social
+	 * @param email
+	 * @param senha
+	 * @return boolean - true para sucesso e excecao para o nao sucesso ao logar
+	 * @throws Exception
+	 */
 	public boolean login(String email, String senha) throws Exception {
 		Usuario usuario = buscaUsuario(email);
 		if(usuario == null)
@@ -73,6 +98,14 @@ public class SystemPop {
 		}
 	}
 	
+	/**
+	 * Metodo para remover um usuario do sistema, caso o usuario esteja 
+	 * cadastro a remocao eh realizada com sucesso senao lanca uma excecao
+	 * com mensagem exibindo o motivo
+	 * @param emailDoUsuario
+	 * @throws LogicaException
+	 * @return void
+	 */
 	public void removeUsuario(String emailDoUsuario) throws LogicaException {
 		Usuario usuarioParaRemover = buscaUsuario(emailDoUsuario);
 		if(usuarioParaRemover == null)
@@ -103,6 +136,18 @@ public class SystemPop {
 		return usuarios;
 	}
 	
+	/**
+	 * Metodo para interagri com o post, onde o usuario pode curtir
+	 * ou rejeitar um post de um amigo. Caso o usuario tente interagir
+	 * com um post nao existente ou que nao seja amigo do dono do post
+	 * entao uma excecao sera lancada.
+	 * 
+	 * @param int - idPost
+	 * @param String - emailAmigo
+	 * @param String - opcao - CURTIR ou REJEITAR
+	 * @throws LogicaException
+	 * @return void
+	 */
 	public void interagirComPost(int idPost, String emailAmigo, String opcao) throws LogicaException {
 		Usuario amigoDoUsuarioLogado = buscaUsuario(emailAmigo);
 		String palavraInteracao = "";
@@ -148,6 +193,13 @@ public class SystemPop {
 		}
 	}
 
+	/**
+	 * Metodo para obter uma informacao especifica do usuario logado.
+	 * Ex: obter nome ou data de nascimento
+	 * @param atributo
+	 * @return String - resultado
+	 * @throws LogicaException
+	 */
 	public String getInfoUsuario(String atributo) throws LogicaException {
 		String retorno = "";
 		if (atributo.equals(NOME)){
@@ -162,6 +214,15 @@ public class SystemPop {
 		return retorno;
 	}
 	
+	/**
+	 * Metodo para obter uma informacao especifica de um usuario cadastrado no 
+	 * sistema.
+	 * Ex: obter nome ou email
+	 * @param String - atributo
+	 * @param String - email
+	 * @return String - resultado
+	 * @throws LogicaException
+	 */
 	public String getInfoUsuario(String atributo, String email) throws LogicaException {
 		Usuario usuario = buscaUsuario(email);
 		String retorno = "";
@@ -180,6 +241,15 @@ public class SystemPop {
 		return retorno;
 	}
 
+	/**
+	 * Metodo para atualizar uma determinada informacao de perfil do usuario
+	 * cadastrado e logado no sistema.
+	 * Ex.: Atualizar senha ou email
+	 * @param atributo
+	 * @param valor
+	 * @throws Exception
+	 * @return void
+	 */
 	public void atualizaPerfil(String atributo, String valor) throws Exception {
 		if (usuarioLogado == null){
 			throw new AtualizaPerfilException("Nao eh possivel atualizar um perfil. Nenhum usuarix esta logadx no +pop.");
@@ -213,6 +283,14 @@ public class SystemPop {
 		return usuarioLogado.getPosts().get(post).getPostString();
 	}
 	
+	/**
+	 * Metodo para obter uma determinada informacao de um post especifico
+	 * do usuario logado no sistema.
+	 * 
+	 * @param String - atributo
+	 * @param int - post
+	 * @return String - resultado
+	 */
 	public String getPost(String atributo, int post) {
 		Post postAtual = usuarioLogado.getPosts().get(post);
 		if (atributo.equals(CONTEUDO)){
@@ -240,6 +318,14 @@ public class SystemPop {
 		return dicionarioHashtags;
 	}
 	
+	/**
+	 * Metodo para um uusario criar um novo post, o post eh adicionado na
+	 * sua lista de posts.
+	 * @param String - mensagem
+	 * @param String - data
+	 * @throws Exception
+	 * @return void
+	 */
 	public void criaPost(String mensagem, String data) throws Exception {
 		if(getUsuarioLogado() == null){
 			throw new LogicaException("Nao eh possivel postar no mural, pois nao ha nenhum usuarix logadx.");		
@@ -308,6 +394,13 @@ public class SystemPop {
 		return usuarioLogado.getNextNotificacao();
 	}
 	
+	/**
+	 * Metodo responsavel por enviar um convite para o futuro amigo,
+	 * 
+	 * @param String - emailFuturoAmigo
+	 * @throws LogicaException
+	 * @return void
+	 */
 	public void adicionaAmigo(String emailFuturoAmigo) throws LogicaException {
 		if (buscaUsuario(emailFuturoAmigo) == null)
 			throw new LogicaException("O usuario " + emailFuturoAmigo + " nao esta cadastrado no +pop.");
@@ -319,7 +412,13 @@ public class SystemPop {
 		candidatoAmigo.adicionaEmailNotificacao(usuarioLogado.getEmail()); //sempre que houver uma notificacao, armazene o email do usuario quem enviou a notificacao
 		
 	}
-	
+	/**
+	 * Metodo responsavel criar a amizade entre os dois usuarios, usando as listas de 
+	 * amigos e as notificacoes para gerenciar a relacao de amizade que sera criada.
+	 * @param emailNovoAmigo
+	 * @throws LogicaException
+	 * @return void
+	 */
 	public void aceitaAmizade(String emailNovoAmigo) throws LogicaException{
 		if (buscaUsuario(emailNovoAmigo) == null)
 			throw new LogicaException("O usuario " + emailNovoAmigo + " nao esta cadastrado no +pop.");
@@ -332,6 +431,15 @@ public class SystemPop {
 		
 	}
 
+	/**
+	 * Metodo responsavel por enviar uma notificacao de rejeicao de amizade,
+	 * quando o usuario que recebeu o convite decide rejeitar a solicitacao
+	 * de amizade.
+	 * 
+	 * @param emailRejeitado
+	 * @throws LogicaException
+	 * @return void
+	 */
 	public void rejeitaAmizade(String emailRejeitado) throws LogicaException {
 		if (buscaUsuario(emailRejeitado) == null)
 			throw new LogicaException("O usuario " + emailRejeitado + " nao esta cadastrado no +pop.");
@@ -343,6 +451,15 @@ public class SystemPop {
 		candidatoAmigo.adicionaNotificacao(usuarioLogado.getNome() + " rejeitou sua amizade."); // envia notificacao para o usuario que mandou o convite
 	}
 	
+	/**
+	 * Metodo que realiza a remocao da amizade entre dois usuarios,
+	 * apos a remocao eh enviada uma notificacao para o usuario que foi excluido da lista 
+	 * de amigos do usuario logado.
+	 * 
+	 * @param emailExAmigo
+	 * @throws LogicaException
+	 * @return void
+	 */
 	public void removeAmigo(String emailExAmigo) throws LogicaException {
 		if (buscaUsuario(emailExAmigo) == null)
 			throw new LogicaException("O usuario " + emailExAmigo + " nao esta cadastrado no +pop.");
@@ -370,6 +487,13 @@ public class SystemPop {
 		return usuarioLogado.getStringPopularidade();
 	}
 	
+	/**
+	 * Metodo para obter a informacao sobre a popularidade que o 
+	 * usuario cadastrado no sistema possui.
+	 * ex: Normal ou IconePop
+	 * @param String - email
+	 * @return String - tipoPopularidade
+	 */
 	public String getTipoPopularidade(String email) {
 		Usuario usuario = buscaUsuario(email);
 		return usuario.getStringPopularidade();
@@ -383,7 +507,15 @@ public class SystemPop {
 		Usuario usuario = buscaUsuario(email);
 		return usuario.getPostEspecifico(post).getPopularidade();
 	}
-
+	
+	/**
+	 * Metodo que retorna a quantidade de curtidas que um post
+	 * especifico do usuario logado recebeu.
+	 * 
+	 * @param int - post
+	 * @return int - qtdCurtidas
+	 * @throws Exception
+	 */
 	public int getCurtidaPorPost(int post) throws Exception {
 		return usuarioLogado.getPostEspecifico(post).getCurtidas();
 	}
@@ -411,11 +543,13 @@ public class SystemPop {
 		return usuario.getPops();
 	}
 	
-	/*
-	 * Método que recupera os
-	 * nomes dos 3 usuários mais 
+	/**
+	 * Metodo que recupera os
+	 * nomes dos 3 usuarios mais 
 	 * populares da rede social
 	 * 
+	 * @param void
+	 * @return String - tresPessasMaisPopulares
 	 */
 	public String getRankingMais() {
 		if (usuarios.size() != 0) {
@@ -427,11 +561,13 @@ public class SystemPop {
 		return "";
 	}
 
-	/*
-	 * Método que recupera os
-	 * nomes dos 3 usuários menos 
+	/**
+	 * Metodo que recupera os
+	 * nomes dos 3 usuarios menos 
 	 * populares da rede social
 	 * 
+	 * @param void
+	 * @return String - tresPessasMenosPopulares
 	 */
 	public String getRankingMenos() {
 		if (usuarios.size() != 0) {
@@ -447,6 +583,11 @@ public class SystemPop {
 		return contadorDeHastags;
 	}
 	
+	/**
+	 * Metodo para atualizar o feed de noticias
+	 * do usuario logado
+	 * @return void
+	 */
 	public void atualizaFeed() {
 		usuarioLogado.atualizaFeed();
 	}
@@ -455,6 +596,14 @@ public class SystemPop {
 		usuarioLogado.atualizaFeedPopularidade();
 	}
 	
+	/**
+	 * Metodo responsavel por salvar em arquivo
+	 * o historico de todos os posts de todos os
+	 * usuarios no sistema.
+	 * 
+	 * @return void
+	 * @throws IOException
+	 */
 	public void salvaHistoricoPosts() throws IOException {
 		List<String> postString = new ArrayList<String>();
 		
@@ -478,6 +627,13 @@ public class SystemPop {
 		
 	}
 	
+	/**
+	 * Metodo responsavel por salvar em arquivo os dados
+	 * dos perfis de todos os usuarios cadastrados no sitema.
+	 * 
+	 * @return void
+	 * @throws IOException
+	 */
 	public void salvaDadosUsuarios() throws IOException{
 		List<String> dadosString = new ArrayList<String>();
 		
